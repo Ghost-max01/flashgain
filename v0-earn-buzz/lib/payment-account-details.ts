30f0df0134ce55470777b5bc483015123b21626d
basework <jimmaamusic@gmail.com>
2026-04-18 09:04:04 +0100

    Update payment-account-details.ts

--- v0-earn-buzz/lib/payment-account-details.ts
+++ v0-earn-buzz/lib/payment-account-details.ts
@@ -8,18 +8,18 @@ export interface PaymentAccountDetails {
 
 const ACCOUNT_DETAILS_BY_USE_CASE: Record<PaymentAccountUseCase, PaymentAccountDetails> = {
   default: {
-    bankName: "Sparkle",
-    accountNumber: "1003105311",
+    bankName: "Fairmoney",
+    accountNumber: "2017833513",
     accountName: "Faith Wali",
   },
   loan: {
-    bankName: "Sparkle Bank",
-    accountNumber: "1003105311",
+    bankName: "Fairmoney",
+    accountNumber: "2017833513",
     accountName: "Faith Wali",
   },
   momo: {
-    bankName: "Sparkle Bank",
-    accountNumber: "1003105311",
+    bankName: "Fairmoney",
+    accountNumber: "2017833513",
     accountName: "Faith Wali",
   },
 }
